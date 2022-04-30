825ea2ffcab6221378f569fb5f82a69a77a674f5
Yonathan Assefa <yab95assefa@gmail.com>
2022-04-30 22:37:26 +0300

    *S* bug-fix

--- HMS/HMS/settings.py
+++ HMS/HMS/settings.py
@@ -15,12 +15,16 @@ from pathlib import Path
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
 
+# load from env file 
+import os
+from dotenv import load_dotenv
+load_dotenv()
 
 # Quick-start development settings - unsuitable for production
 # See https://docs.djangoproject.com/en/4.0/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'django-insecure--#wbenc4#z0m5(_cpqr&ce%#^#1i0p@5y(0^77j%8h9exn98+-'
+SECRET_KEY = os.getenv('SECRET_KEY')
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
@@ -97,12 +101,12 @@ WSGI_APPLICATION = 'HMS.wsgi.application'
 # connect to mysql database
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.mysql',
-        'NAME': 'hms',
-        'USER': 'iris',
-        'PASSWORD': 'Yon@3246',
-        'HOST': 'localhost',
-        'PORT': '3306',
+        'ENGINE': os.getenv('DB_ENGINE'),
+        'NAME': os.getenv('DB_NAME'),
+        'USER': os.getenv('DB_USER'),
+        'PASSWORD': os.getenv('DB_PASSWORD'),
+        'HOST': os.getenv('DB_HOST'),
+        'PORT': os.getenv('DB_PORT'),
     }
 }
 
